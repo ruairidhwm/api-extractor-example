832bae788dbb322c80dd9d05dc04865d06352848
Ruairidh Wynne-McHardy <ruairidhwm@gmail.com>
2020-12-07 21:41:38 +0000

    Docs: Description

--- dist/api-extractor-example.d.ts
+++ dist/api-extractor-example.d.ts
@@ -8,6 +8,8 @@ export declare type Bar = string;
  *
  * @param message - Tells you a thing is cool.
  * @public
+ * @description
+ * This appends '- cool huh?' to the message that you pass through.
  */
 export declare const CoolFunction: (message: string) => string;
 
